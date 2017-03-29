d887b0a9f08d98ab495283afc58c93e9c89b6714
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-29 18:21:32 +0700

    add js

--- js/all-script.js
+++ js/all-script.js
@@ -1,7 +1,10 @@
-var ROOT_URL = 'https://ft-umj-4.github.io/story';
 
-$('.article__content>img').each(function(){
-	var firstSrc = 	$(this).attr('src');
-
-    $(this).attr('src', ROOT_URL + firstSrc);
+$(window).on('load', function(){   
+   	$('.article__content>img').each(function(){
+		var firstSrc = 	$(this).attr('src');
+		if(!ROOT_URL){
+			ROOT_URL = 'https://ft-umj-4.github.io/story';
+		}
+	    $(this).attr('src', ROOT_URL + firstSrc);
+	});
 });
