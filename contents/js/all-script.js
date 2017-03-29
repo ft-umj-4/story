3dc39d0f5703235413ddcfbc7b621dfbd1b1806c
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-29 18:25:02 +0700

    modify script

--- contents/js/all-script.js
+++ contents/js/all-script.js
@@ -1,6 +1,6 @@
 
 $(window).on('load', function(){   
-   	$('.article__content>img').each(function(){
+   	$('.article__content img').each(function(){
 		var firstSrc = 	$(this).attr('src');
 		if(!ROOT_URL){
 			ROOT_URL = 'https://ft-umj-4.github.io/story';
