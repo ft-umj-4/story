1b03bc3983256f54bd240fe5d78ef22ed81e8e01
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-31 18:50:56 +0700

    update master

---
+++ js/all-script.js
@@ -0,0 +1,10 @@
+
+$(window).on('load', function(){   
+   	$('.article__content img').each(function(){
+		var firstSrc = 	$(this).attr('src');
+		if(!ROOT_URL){
+			ROOT_URL = 'https://ft-umj-4.github.io/story';
+		}
+	    $(this).attr('src', ROOT_URL + firstSrc);
+	});
+});
