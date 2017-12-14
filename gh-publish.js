eb8244708ff29f8fff16a688a26194b0b145b9b6
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-12-14 20:18:31 +0700

    fix gh-publish.js

--- gh-publish.js
+++ gh-publish.js
@@ -1,9 +1,11 @@
-var ghpages = require('gh-pages');
-var path = require('path');
+var ghpages = require('gh-pages')
+var path = require('path')
+var moment = require('moment')
  
 console.log('start publishing to master...')
-ghpages.publish('dist', {
-  branch: 'master'
+ghpages.publish('build', {
+  branch: 'master',
+  message: 'Merge to master from develop in ' + moment(new Date()).format('MMMM Do YYYY, h:mm:ss a')
 }, function(err) {
   console.log('done publishing to master')
 });
